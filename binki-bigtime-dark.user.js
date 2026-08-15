52d858c28482e00965db3c8e26a2d13992cb28ef
Nathan Phillip Brink <ohnobinki@ohnopublishing.net>
2026-08-14 21:19:34 -0400

    binki-bigtime-dark-1.0.4

--- binki-bigtime-dark.user.js
+++ binki-bigtime-dark.user.js
@@ -2,7 +2,7 @@
 // @name binki-bigtime-dark
 // @description A system theme-conditional dark theme for BigTime.
 // @homepageURL https://github.com/binki/binki-bigtime-dark
-// @version 1.0.3
+// @version 1.0.4
 // @match https://*.bigtime.net/bigtime
 // @match https://*.bigtime.net/bigtime/*
 // @match https://*.bigtime.net/Bigtime
